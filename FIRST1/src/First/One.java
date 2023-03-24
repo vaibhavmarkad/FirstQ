7d4e07909129ab6c3b1c3a7582dda29415ce8076
Vaibhav Annasaheb Markad <Lenovo@DESKTOP-28S5KMI>
2023-03-24 12:43:23 +0530

    First Question

---
+++ FIRST1/src/First/One.java
@@ -0,0 +1,30 @@
+package First;
+
+public class One {
+	public static void main(String args[])   
+	{  
+	String st="vaibhav";  
+	int i, length;  
+	int counters[] = new int[150];  
+	  
+	
+	
+	 
+	length= st.length();  
+	
+	for (i = 0; i < length; i++)   
+	{  
+	counters[(int) st.charAt(i)]++;  
+	}  
+	
+	for (i = 0; i < 150; i++)   
+	{  
+	if (counters[i] != 0)   
+	{  
+	   
+	System.out.println((char) i + " = " + counters[i]);  
+	}  
+	}  
+	}
+
+}
